09597818858115e615954a2088df70fe92571d92
magic smoke releaser <1f604@users.noreply.github.com>
2023-03-22 16:51:04 +0000

    Update tests.py

--- python/tests.py
+++ python/tests.py
@@ -1,7 +1,8 @@
 import unittest
-from base51a import b51a_validate, b51a_remap
+from base51a import b51a_validate, b51a_remap, b51a_generate_random
 
 import string
+from collections import defaultdict
 illegal_characters = set("O91IlWwmncd")
 illegal_pairs = set(["VV", "vv"])
 legal_characters = set(string.ascii_letters + string.digits) - illegal_characters
@@ -47,15 +48,32 @@ class TestBase51aValidationMethod(unittest.TestCase):
 			result = b51a_validate(ip + "aa")
 			self.assertEqual(result, "Error: illegal pair: "+ip)
 
-
 class TestBase51aRemapMethod(unittest.TestCase):
 	def test_remapping(self):
 		result = b51a_remap("aa9a999mOoOO0O909g090Oadsfd")
 		self.assertEqual(result,'aagagggm0o0000g0gg0g00adsfd')
 
-
-
-
-
-
+class TestBase51aGenerateMethod(unittest.TestCase):
+	def test_generates_no_illegals(self):
+		for n in range(5): # check zero length strings too!
+			for _ in range(20000):
+				rs = b51a_generate_random(n)
+				self.assertEqual(b51a_validate(rs), True)
+	
+	def test_generates_all_legal_chars(self):
+		for n in range(1,5):
+			seen = defaultdict(bool)
+			for _ in range(2000):
+				rs = b51a_generate_random(n)
+				for c in rs:
+					seen[c] = True
+			for c in legal_characters:
+				self.assertTrue(seen[c], (c, seen))
+			
+	def test_generates_different_strings(self):
+		results = set()
+		for _ in range(100):
+			rs = b51a_generate_random(8)
+			results.add(rs)
+		self.assertEqual(len(results), 100) # should never get repeats.
 			
